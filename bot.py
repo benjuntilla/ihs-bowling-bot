06afc6fb3b62ab76f78d3f09f1064bcd1fd1f4f1
Chelston Juntilla <chejun4790@gmail.com>
2020-08-01 17:04:49 -0700

    Refactor, automatically remove members from brig

--- bot.py
+++ bot.py
@@ -18,6 +18,11 @@ def is_in_dms(ctx):
     return True
 
 
+async def try_system_message(guild, message):
+    if guild.system_channel is not None:
+        await guild.system_channel.send(message)
+
+
 class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -39,6 +44,12 @@ class General(commands.Cog):
 class Administration(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.update_brig_members.start()
+        with open("bot_state.json", "r") as file:
+            self.state_data = json.load(file)
+
+    def cog_unload(self):
+        self.update_brig_members.cancel()
 
     def cog_check(self, ctx):
         if ctx.guild is None:
@@ -49,44 +60,63 @@ class Administration(commands.Cog):
 
     @commands.command(brief="Put someone in the brig")
     async def brig(self, ctx, member: discord.Member, duration: typing.Optional[int]):
+        await self.add_to_brig(ctx.guild, member, duration)
+
+    @commands.command(brief="Remove someone from the brig")
+    async def unbrig(self, ctx, member: discord.Member):
+        await self.remove_from_brig(ctx.guild, member)
+
+    @tasks.loop(seconds=1.0)
+    async def update_brig_members(self):
+        for guild, data in self.state_data.items():
+            for member, sentence in data["brigMembers"].items():
+                if sentence[1] != 0 and sentence[1] < time.time():
+                    guild = self.bot.get_guild(int(guild))
+                    member = guild.get_member(int(member))
+                    await self.remove_from_brig(guild, member)
+
+    async def remove_from_brig(self, guild: discord.Guild, member: discord.Member):
         with open("bot_state.json", "r+") as file:
-            data = json.load(file)
-            guild_data = data.get(ctx.guild.id, {})
+            self.state_data = json.load(file)
+            self.state_data[str(guild.id)]["brigMembers"].pop(str(member.id), None)
+            file.seek(0)
+            json.dump(self.state_data, file)
+            file.truncate()
+        role = discord.utils.get(guild.roles, name="THE BRIG")
+        if role is None:
+            message = "No role named \"THE BRIG\" exists! Please create one before using `unbrig`."
+            await try_system_message(guild, message)
+            return
+        else:
+            await member.remove_roles(role, reason="Removed from the brig")
+            message = "Removed {0} from the brig.".format(member.mention)
+            await try_system_message(guild, message)
+
+    async def add_to_brig(self, guild: discord.Guild, member: discord.Member, duration: typing.Optional[int]):
+        with open("bot_state.json", "r+") as file:
+            self.state_data = json.load(file)
+            guild_data = self.state_data.get(str(guild.id), {})
             brig_dict1 = {member.id: [time.time(), time.time() + (duration * 60)] if duration else [0, 0]}
             brig_dict2 = guild_data.get("brigMembers", {})
             guild_data["brigMembers"] = {**brig_dict1, **brig_dict2}
-            data[ctx.guild.id] = guild_data
+            self.state_data[str(guild.id)] = guild_data
             file.seek(0)
-            json.dump(data, file)
+            json.dump(self.state_data, file)
             file.truncate()
-        role = discord.utils.get(ctx.guild.roles, name="THE BRIG")
+        role = discord.utils.get(guild.roles, name="THE BRIG")
         if role is None:
-            await ctx.send("No role named \"THE BRIG\" exists! Please create one before using this command.")
+            message = "No role named \"THE BRIG\" exists! Please create one before using this command."
+            await try_system_message(guild, message)
             return
         else:
             # TODO: Add event logging to file
             # TODO: Add event logging to server
             await member.add_roles(role, reason="Put in the brig")
-            await ctx.send("Added {0} to the brig for {1} minutes."
-                           .format(member.mention, duration if duration is not None else "indefinite"))
+            message = "Added {0} to the brig for {1} minutes."\
+                .format(member.mention, duration if duration is not None else "indefinite")
+            await try_system_message(guild, message)
 
-    @commands.command(brief="Remove someone from the brig")
-    async def unbrig(self, ctx, member: discord.Member):
-        with open("bot_state.json", "r+") as file:
-            data = json.load(file)
-            data[str(ctx.guild.id)]["brigMembers"].pop(str(member.id), None)
-            file.seek(0)
-            json.dump(data, file)
-            file.truncate()
-        role = discord.utils.get(ctx.guild.roles, name="THE BRIG")
-        if role is None:
-            await ctx.send("No role named \"THE BRIG\" exists! Please create one before using this command.")
-            return
-        else:
-            await member.remove_roles(role, reason="Removed from the brig")
-            await ctx.send("Removed {0} from the brig.".format(member.mention))
-
-    # TODO: automatically remove people from the brig
+    # TODO: Add command for showing brig members
 
 
 allowed_errors = {NotAdministrator, commands.NoPrivateMessage, commands.MissingRequiredArgument,
