94cb7db51230b1b5c74e56f15cce76ad63c7df27
Chelston Juntilla <chejun4790@gmail.com>
2020-08-03 16:59:09 -0700

    Move to PostgreSQL for persistence, update Pipfile

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import discord
 from discord.ext import commands, tasks
 import json
 import os
+import psycopg2
 from pytz import timezone
 import time
 import typing
@@ -26,8 +27,12 @@ async def try_system_message(guild, message):
         await guild.system_channel.send(message)
 
 
-def convert_unix_tz(time):
-    return datetime.fromtimestamp(time, tz=timezone("America/Phoenix")).strftime('%d/%m/%Y, %I:%M %p')
+def epoch_to_postgresql(timestamp):
+    return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def datetime_to_phoenix(timestamp):
+    return timestamp.replace(tzinfo=timezone("America/Phoenix")).strftime('%d/%m/%Y, %I:%M %p')
 
 
 class General(commands.Cog):
@@ -51,12 +56,25 @@ class General(commands.Cog):
 class Administration(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+        with self.conn, self.conn.cursor() as cur:
+            sql = '''
+            CREATE TABLE IF NOT EXISTS brigged_members (
+                member_id bigint,
+                guild_id bigint,
+                brig_start timestamp NULL,
+                brig_end timestamp NULL,
+                PRIMARY KEY (member_id, guild_id)
+            );
+            '''
+            cur.execute(sql)
         self.update_brig_members.start()
         with open("bot_state.json", "r") as file:
             self.state_data = json.load(file)
 
     def cog_unload(self):
         self.update_brig_members.cancel()
+        self.conn.close()
 
     def cog_check(self, ctx):
         if ctx.guild is None:
@@ -66,9 +84,9 @@ class Administration(commands.Cog):
         return True
 
     @commands.command(brief="Put someone in the brig")
-    async def brig(self, ctx, member: discord.Member, duration: typing.Optional[float]):
+    async def brig(self, ctx, member: discord.Member, duration: typing.Optional[int]):
         try:
-            convert_unix_tz(time.time() + (duration * 60))
+            epoch_to_postgresql(time.time() + (duration * 60))
         except:
             await ctx.send(phrases["invalid_num_input"])
             return
@@ -80,35 +98,36 @@ class Administration(commands.Cog):
 
     @commands.command(brief="List brig members and their sentences")
     async def listbrig(self, ctx):
-        brig_members = self.state_data[str(ctx.guild.id)]["brigMembers"]
-        if brig_members == {}:
+        with self.conn, self.conn.cursor() as cur:
+            sql = "SELECT * FROM brigged_members WHERE guild_id = %s;"
+            cur.execute(sql, (ctx.guild.id,))
+            brig_members = cur.fetchall()
+        if len(brig_members) == 0:
             await ctx.send(phrases["brig_empty"])
             return
         embed = discord.Embed(title="The Brig", color=discord.Color.orange())
-        for member_id, duration in brig_members.items():
-            member = ctx.guild.get_member(int(member_id))
+        for row in brig_members:
+            member = ctx.guild.get_member(row[0])
             duration_formatted = "{0} — {1} ({2} minutes)".format(
-                convert_unix_tz(duration[0]), convert_unix_tz(duration[1]), round((duration[1] - duration[0]) / 60, 2)
-            ) if duration[1] != 0 else "Indefinite"
+                datetime_to_phoenix(row[2]), datetime_to_phoenix(row[3]),
+                round((row[3] - row[2]).total_seconds() / 60)
+            ) if row[2] is not None else "Indefinite"
             embed.add_field(name=member.name + "#" + member.discriminator, value=duration_formatted, inline=False)
         await ctx.send(embed=embed)
 
     @tasks.loop(seconds=1.0)
     async def update_brig_members(self):
-        for guild, data in self.state_data.items():
-            for member, sentence in data["brigMembers"].items():
-                if sentence[1] != 0 and sentence[1] < time.time():
-                    guild = self.bot.get_guild(int(guild))
-                    member = guild.get_member(int(member))
-                    await self.remove_from_brig(guild, member)
+        with self.conn, self.conn.cursor() as cur:
+            cur.execute("SELECT * FROM brigged_members;")
+            brig_members = cur.fetchall()
+        for row in brig_members:
+            if row[3] is not None and row[3].timestamp() < time.time():
+                guild = self.bot.get_guild(row[1])
+                member = guild.get_member(row[0])
+                await self.remove_from_brig(guild, member)
 
     async def remove_from_brig(self, guild: discord.Guild, member: discord.Member):
-        with open("bot_state.json", "r+") as file:
-            self.state_data = json.load(file)
-            self.state_data[str(guild.id)]["brigMembers"].pop(str(member.id), None)
-            file.seek(0)
-            json.dump(self.state_data, file)
-            file.truncate()
+        # Remove role
         role = discord.utils.get(guild.roles, name="THE BRIG")
         if role is None:
             message = phrases["no_role"].format("\"THE BRIG\"", "`unbrig`")
@@ -118,30 +137,34 @@ class Administration(commands.Cog):
             await member.remove_roles(role, reason="Removed from the brig")
             message = phrases["brig_remove"].format(member.mention)
             await try_system_message(guild, message)
+        # Update db
+        with self.conn, self.conn.cursor() as cur:
+            sql = "DELETE FROM brigged_members WHERE member_id = %s AND guild_id = %s;"
+            cur.execute(sql, (member.id, guild.id))
 
-    async def add_to_brig(self, guild: discord.Guild, member: discord.Member, duration: typing.Optional[float]):
-        with open("bot_state.json", "r+") as file:
-            self.state_data = json.load(file)
-            guild_data = self.state_data.get(str(guild.id), {})
-            brig_dict1 = {str(member.id): [time.time(), time.time() + (duration * 60)] if duration else [0, 0]}
-            brig_dict2 = guild_data.get("brigMembers", {})
-            guild_data["brigMembers"] = {**brig_dict2, **brig_dict1}
-            self.state_data[str(guild.id)] = guild_data
-            file.seek(0)
-            json.dump(self.state_data, file)
-            file.truncate()
+    async def add_to_brig(self, guild: discord.Guild, member: discord.Member, duration: typing.Optional[int]):
+        # Add role
         role = discord.utils.get(guild.roles, name="THE BRIG")
         if role is None:
             message = phrases["no_role"].format("\"THE BRIG\"", "`brig`")
             await try_system_message(guild, message)
             return
         else:
-            # TODO: Add event logging to file
-            # TODO: Add event logging to server
             await member.add_roles(role, reason="Put in the brig")
             message = phrases["brig_add"]\
                 .format(member.mention, "for " + str(duration) + " minutes" if duration else "indefinitely")
             await try_system_message(guild, message)
+        # Update db
+        with self.conn, self.conn.cursor() as cur:
+            sql = '''
+            INSERT INTO brigged_members VALUES (%s, %s, %s, %s)
+            ON CONFLICT (member_id, guild_id) DO UPDATE SET 
+                brig_start = excluded.brig_start,
+                brig_end = excluded.brig_end;
+            '''
+            brig_start = epoch_to_postgresql(time.time())
+            brig_end = epoch_to_postgresql(time.time() + (duration * 60))
+            cur.execute(sql, (member.id, guild.id, brig_start, brig_end))
 
 
 allowed_errors = {NotAdministrator, commands.NoPrivateMessage, commands.MissingRequiredArgument,
@@ -160,7 +183,6 @@ async def on_command_error(ctx, error):
     if is_allowed_error(error):
         await ctx.send(error)
     else:
-        # TODO: Add error logging to file
         await ctx.send(phrases["unknown_issue"])
         raise error
 
